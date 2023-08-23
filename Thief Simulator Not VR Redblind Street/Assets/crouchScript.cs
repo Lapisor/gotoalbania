ea36b7377516e503254a82487042e18abe116f26
MeEqualsGurl <cassidylyle@proton.me>
2023-08-23 16:45:04 +0100

    alpha-v1.1

---
+++ Thief Simulator Not VR Redblind Street/Assets/crouchScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class crouchScript : MonoBehaviour
+{
+
+    public bool isCrouching;
+
+    public float crouchedSpeed;
+    public float regSpeed;
+
+    public float crouchedSize;
+    public float regSize;
+
+    public jacobAI Jacob;
+    public float regDetection;
+    public float crouchedDetection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            this.GetComponent<FirstPersonController>().m_WalkSpeed = crouchedSpeed;
+            this.GetComponent<FirstPersonController>().m_RunSpeed = crouchedSpeed * 2;
+            this.GetComponent<CharacterController>().height = crouchedSize;
+            Jacob.mTargetDetectionDistance = crouchedDetection;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            this.GetComponent<FirstPersonController>().m_WalkSpeed = regSpeed;
+            this.GetComponent<FirstPersonController>().m_RunSpeed = regSpeed * 2;
+            this.GetComponent<CharacterController>().height = regSize;
+            Jacob.mTargetDetectionDistance = regDetection;
+        }
+    }
+}
---
+++ Thief Simulator Not VR Redblind Street/Assets/mouseHider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseHider : MonoBehaviour
+{
+
+    public bool mouseHidden;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseHidden)
+        {
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
+    }
+}
