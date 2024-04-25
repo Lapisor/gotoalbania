3aea3931231c50eb7bd89b8ace931d1070102bd4
WPE-Official <wpecentralcommittee@proton.me>
2024-04-25 11:49:40 +0100

    Realtime Update + Performance Optimisation

---
+++ Thief Simulator Not VR Redblind Street/Assets/torch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torch : MonoBehaviour
+{
+
+    public GameObject Torch;
+    public bool On;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (On)
+            {
+                Torch.SetActive(false);
+                On = false;
+            }
+            else
+            {
+                Torch.SetActive(true);
+                On = true;
+            }
+        }
+    }
+}
