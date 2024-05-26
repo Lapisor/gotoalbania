15bfe421faac59d6e99101e091ab4faffb8aecff
MeEqualsGurl <cassidylyle@proton.me>
2024-05-26 23:11:31 +0100

    CONTROLLER COUNTS AS INPUT (Comrades)

--- Thief Simulator Not VR Redblind Street/Assets/torch.cs
+++ Thief Simulator Not VR Redblind Street/Assets/torch.cs
@@ -17,7 +17,7 @@ public class torch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetButtonDown("Torch"))
         {
             if (On)
             {
